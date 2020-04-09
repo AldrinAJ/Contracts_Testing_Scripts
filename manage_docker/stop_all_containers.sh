a84fb5126bfbc2f387be58bd15e6c7e1297e3d1d
Jeongmin Kim <appff13@gmail.com>
2020-04-08 18:37:50 -0700

    modify script name add script to stop

---
+++ manage_docker/stop_all_containers.sh
@@ -0,0 +1,19 @@
+echo "Starting Oyente..."
+docker stop oyente
+echo "Complete"
+
+echo "Starting Manticore..."
+docker stop manticore
+echo "Complete"
+
+echo "Starting Mythril..."
+docker stop mythril
+echo "Complete"
+
+echo "Starting Securfy..."
+docker stop securify
+echo "Complete"
+
+echo "Starting Slither..."
+docker stop slither
+echo "Complete"
