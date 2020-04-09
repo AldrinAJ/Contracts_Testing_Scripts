87749559180dcba36eed7121ff21dbedc7683779
Jeongmin Kim <appff13@gmail.com>
2020-04-08 18:35:17 -0700

    add a script to start all containers

---
+++ manage_docker/starting_all_containers.sh
@@ -0,0 +1,19 @@
+echo "Starting Oyente..."
+docker start oyente
+echo "Complete"
+
+echo "Starting Manticore..."
+docker start manticore
+echo "Complete"
+
+echo "Starting Mythril..."
+docker start mythril
+echo "Complete"
+
+echo "Starting Securfy..."
+docker start securify
+echo "Complete"
+
+echo "Starting Slither..."
+docker start slither
+echo "Complete"
