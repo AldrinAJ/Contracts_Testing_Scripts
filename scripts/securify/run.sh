f4fbb921636885f8d10784216fe066bbd814f5b9
Jeongmin Kim <appff13@gmail.com>
2020-04-28 19:37:09 -0700

    Add and test slither run.sh Fix bug of securify run.sh

--- scripts/securify/run.sh
+++ scripts/securify/run.sh
@@ -1,2 +1,2 @@
 cd /app/Contracts_Testing_Scripts/test_contracts/securify
-securify $1 > ../../results/securify/$2
+securify $1 2> ../../results/securify/$2
---
+++ scripts/slither/run.sh
@@ -0,0 +1,2 @@
+cd /app/Contracts_Testing_Scripts/test_contracts/slither
+slither $1 2> ../../results/slither/$2
