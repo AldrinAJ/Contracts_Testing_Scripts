cfefe4e811e245599df1f44485640ba9fb3b6eae
Jeongmin Kim <appff13@gmail.com>
2020-04-28 22:30:29 -0700

    Add contract files

---
+++ test_contracts/securify/1_securify.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.12;
+
+contract IntermediaryCallback {
+    function registerIntermediary(address what) public payable;
+    // for solidity 0.4.21
+    /*function registerIntermediary(address what) public payable;*/
+}
