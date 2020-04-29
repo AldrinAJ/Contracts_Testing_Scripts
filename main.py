1d4ecd9eb652aaf568ac8ded8547115b10343228
Jeongmin Kim <appff13@gmail.com>
2020-04-28 21:37:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
 
-if len(sys.argv) != 3:
-	print "Usage: main.py containerID solidityName"
-else:
-	contId = "mythril"
-	solidityName = "1_oyente"
-	scriptPath = "/app/Contracts_Testing_Scripts/scripts/" + contId + "/run.sh"
-	docker_exec = "docker exec -it " + contId + " bash " + scriptPath + " " + solidityName + ".sol " + solidityName + ".out"
+def main():
 
-	print docker_exec
+	if len(sys.argv) != 3:
+		print "Usage: main.py containerID solidityName"
+	else:
+		contId = sys.argv[1]
+		solidityName = sys.argv[2]
+		scriptPath = "/app/Contracts_Testing_Scripts/scripts/" + contId + "/run.sh"
+		docker_exec = "docker exec -it " + contId + " bash " + scriptPath + " " + solidityName + ".sol " + solidityName + ".out"
+		print docker_exec
+
+if __name__ == "__main__":
+	main()
