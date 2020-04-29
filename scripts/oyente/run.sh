3129ea82544426d4ec71164af3537fd3b3f6fe1b
Jeongmin Kim <appff13@gmail.com>
2020-04-28 22:04:07 -0700

    Fix run.sh for oyente

--- scripts/oyente/run.sh
+++ scripts/oyente/run.sh
@@ -1,2 +1,3 @@
 cd /app/Contracts_Testing_Scripts/test_contracts/oyente
-python /oyente/oyente/oyente.py -s ./$1 2> ../../results/oyente/$2
+pwd
+python /oyente/oyente/oyente.py -ce -s ./$1 2> ../../results/oyente/$2
