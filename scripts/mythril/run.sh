ddc36a8a1eaa55447af58140c873ecae87270d08
Jeongmin Kim <appff13@gmail.com>
2020-04-28 19:20:18 -0700

    Add and test mythril run.sh Add manticore run.sh

---
+++ scripts/mythril/run.sh
@@ -0,0 +1,2 @@
+cd /app/Contracts_Testing_Scripts/test_contracts/mythril/
+myth a $1 > ../../results/mythril/$2
--- test_contracts/manticore/run.sh
+++ test_contracts/manticore/run.sh
@@ -1,1 +1,2 @@
-manticore $1
+cd /app/Contracts_Testing_Scripts/test_contracts/manticore
+manticore $1 > ../../results/manticore/$2
