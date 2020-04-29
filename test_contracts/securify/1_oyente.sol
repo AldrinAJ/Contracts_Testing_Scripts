a6127de85b9ef9d0dc5e8a6745d72255563ca330
Jeongmin Kim <appff13@gmail.com>
2020-04-28 22:24:47 -0700

    Update solidity file name according to naming rule

--- test_contracts/securify/1_oyente.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.5.12;
-
-contract IntermediaryCallback {
-    function registerIntermediary(address what) public payable;
-    // for solidity 0.4.21
-    /*function registerIntermediary(address what) public payable;*/
-}
