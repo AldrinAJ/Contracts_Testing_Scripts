b31396f1544e0896d804fb881ffbaaa43ec89047
Jeongmin Kim <appff13@gmail.com>
2020-04-28 22:08:07 -0700

    Change manticore run.sh to make the results to be placed in results/manticore

--- scripts/manticore/run.sh
+++ scripts/manticore/run.sh
@@ -1,2 +1,2 @@
-cd /app/Contracts_Testing_Scripts/test_contracts/manticore
-manticore $1 > ../../results/manticore/$2
+cd /app/Contracts_Testing_Scripts/results/manticore/
+manticore ../../test_scripts/manticore/$1 > ../../results/manticore/$2
