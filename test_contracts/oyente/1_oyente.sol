8c0d09736857a5bc283c77386b5e3b1ed1175ab7
Jeongmin Kim <appff13@gmail.com>
2020-05-03 00:01:28 -0700

    Add and Test IntegerOverflow oyente.sol

--- test_contracts/oyente/1_oyente.sol
+++ test_contracts/oyente/1_oyente.sol
@@ -1,11 +1,23 @@
 contract IntegerOverflow {
-
     mapping (address => uint256) public balanceOf;
-        
+    function getCustom () public returns (uint256) {
+        return 444;
+    }
     function transfer(address _to, uint256 _value) public {
         require(balanceOf[msg.sender] >= _value);
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
     }
+}
 
+contract Alice {
+    mapping (address => uint256) public balanceOf;
+    address _t;
+    IntegerOverflow io = IntegerOverflow(_t);
+
+    function transfer(address _to, uint256 _value) public {
+       uint256 sample = io.getCustom();
+       balanceOf[_to] += sample + 1;
+       //balanceOf[_to] += sample + value;
+    }
 }
