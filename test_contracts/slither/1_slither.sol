095280f7e1562b703de3a6c8796cd40510542ddb
Jeongmin Kim <appff13@gmail.com>
2020-05-03 00:58:10 -0700

    Add slither solidity but slither does not check the integeroverflow

--- test_contracts/slither/1_slither.sol
+++ test_contracts/slither/1_slither.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.6.4;
+
 contract IntegerOverflow {
     mapping (address => uint256) public balanceOf;
     function getCustom () public returns (uint256) {
@@ -7,12 +9,11 @@ contract IntegerOverflow {
 
 contract Alice {
     mapping (address => uint256) public balanceOf;
-    address _t;
+    address constant _t = 0xE0f5206BBD039e7b0592d8918820024e2a7437b9;
     IntegerOverflow io = IntegerOverflow(_t);
 
-    function transfer(address _to, uint256 _value) public {
+    function transfer(address _to) public {
        uint256 sample = io.getCustom();
        balanceOf[_to] += sample + 1;
-       //balanceOf[_to] += sample + _value;
     }
 }
--- test_contracts/slither/2_slither.sol
+++ test_contracts/slither/2_slither.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.6.4;
+
 contract IntegerOverflow {
     mapping (address => uint256) public balanceOf;
     function getCustom () public returns (uint256) {
@@ -7,11 +9,11 @@ contract IntegerOverflow {
 
 contract Alice {
     mapping (address => uint256) public balanceOf;
-    address _t;
+    address constant _t = 0xE0f5206BBD039e7b0592d8918820024e2a7437b9;
     IntegerOverflow io = IntegerOverflow(_t);
 
-    function transfer(address _to, uint256 _value) public {
+    function transfer(address _to, uint256 _v) public {
        uint256 sample = io.getCustom();
-       balanceOf[_to] += sample + _value;
+       balanceOf[_to] += sample + _v;
     }
 }
