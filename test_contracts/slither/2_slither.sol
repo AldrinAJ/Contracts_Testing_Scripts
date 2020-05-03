86a0835b0aaafb035e7feee821910aef6ece21e7
Jeongmin Kim <appff13@gmail.com>
2020-05-03 00:25:46 -0700

    ADD slither 2

---
+++ test_contracts/slither/2_slither.sol
@@ -0,0 +1,17 @@
+contract IntegerOverflow {
+    mapping (address => uint256) public balanceOf;
+    function getCustom () public returns (uint256) {
+        return 444;
+    }
+}
+
+contract Alice {
+    mapping (address => uint256) public balanceOf;
+    address _t;
+    IntegerOverflow io = IntegerOverflow(_t);
+
+    function transfer(address _to, uint256 _value) public {
+       uint256 sample = io.getCustom();
+       balanceOf[_to] += sample + _value;
+    }
+}
