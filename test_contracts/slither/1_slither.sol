52d6df2a2576aeb1a8594d99df459f99cc79bf82
Jeongmin Kim <appff13@gmail.com>
2020-05-03 00:25:29 -0700

    Add sliter test code

--- test_contracts/slither/1_slither.sol
+++ test_contracts/slither/1_slither.sol
@@ -1,7 +1,18 @@
-pragma solidity ^0.6.4;
+contract IntegerOverflow {
+    mapping (address => uint256) public balanceOf;
+    function getCustom () public returns (uint256) {
+        return 444;
+    }
+}
 
-contract IntermediaryCallback {
-    function registerIntermediary(address what) public payable;
-    // for solidity 0.4.21
-    /*function registerIntermediary(address what) public payable;*/
+contract Alice {
+    mapping (address => uint256) public balanceOf;
+    address _t;
+    IntegerOverflow io = IntegerOverflow(_t);
+
+    function transfer(address _to, uint256 _value) public {
+       uint256 sample = io.getCustom();
+       balanceOf[_to] += sample + 1;
+       //balanceOf[_to] += sample + _value;
+    }
 }
