3120d21bc489d18fd331ff758c9be8156409f1ee
Jeongmin Kim <appff13@gmail.com>
2020-05-03 18:09:23 -0700

    ADD underflow sample for oyente

---
+++ test_contracts/oyente/4_oyente.sol
@@ -0,0 +1,17 @@
+contract IntegerUnderflow {
+    mapping (address => uint256) public balanceOf;
+    function getCustom () public returns (uint256) {
+        return 0xFFFFFFFFFFFFFFF;
+    }
+}
+
+contract Alice {
+    mapping (address => uint256) public balanceOf;
+    address _t;
+    IntegerUnderflow io = IntegerUnderflow(_t);
+
+    function transfer(address _to, uint256 _value) public {
+       uint256 sample = io.getCustom();
+       balanceOf[_to] += sample - value;
+    }
+}
