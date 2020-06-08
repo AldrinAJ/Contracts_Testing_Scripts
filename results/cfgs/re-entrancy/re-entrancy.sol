838f145c91f03f25f26a34dec180d6b8fdee10de
Jeongmin Kim <appff13@gmail.com>
2020-06-07 21:14:32 -0700

    Add dots for re-entrancy

--- results/cfgs/re-entrancy/re-entrancy.sol
+++ results/cfgs/re-entrancy/re-entrancy.sol
@@ -1,11 +1,12 @@
 contract DAO
 {
     mapping (address => uint) private userBalances;
+    bool success = true;
 
     function withdrawBalance() public {
             if (true)
             {
-                (bool success, ) = msg.sender.call.value(amountToWithdraw)("");
+                someFunc();
                 require(success);
                 uint amountToWithdraw = userBalances[msg.sender];
                 userBalances[msg.sender] = 0;
@@ -14,5 +15,10 @@ contract DAO
             {
                 userBalances[msg.sender] = 100;
             }
+    }
+
+    function someFunc() private
+    {
+        require (success);
     }
 }
