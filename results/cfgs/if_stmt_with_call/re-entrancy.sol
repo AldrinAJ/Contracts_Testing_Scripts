5f25b0c3fd9e245598738619e3fef9f9bfbd8092
Jeongmin Kim <appff13@gmail.com>
2020-06-09 13:13:45 +0900

    Code alignment

--- results/cfgs/if_stmt_with_call/re-entrancy.sol
+++ results/cfgs/if_stmt_with_call/re-entrancy.sol
@@ -3,18 +3,19 @@ contract DAO
     mapping (address => uint) private userBalances;
     bool success = true;
 
-    function withdrawBalance() public {
-            if (true)
-            {
-                someFunc();
-                require(success);
-                uint amountToWithdraw = userBalances[msg.sender];
-                userBalances[msg.sender] = 0;
-            }
-            else
-            {
-                userBalances[msg.sender] = 100;
-            }
+    function withdrawBalance() public 
+    {
+        if (true)
+        {
+            someFunc();
+            require(success);
+            uint amountToWithdraw = userBalances[msg.sender];
+            userBalances[msg.sender] = 0;
+        }
+        else
+        {
+            userBalances[msg.sender] = 100;
+        }
     }
 
     function someFunc() private
