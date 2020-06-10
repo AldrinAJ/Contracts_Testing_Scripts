4bbf7b6e7df3e1ef611f3bccba0a8a6d058ffe0f
Jeongmin Kim <appff13@gmail.com>
2020-06-09 18:27:57 -0700

    Update run.sh to remove dot files after moving our repo folder

--- scripts/buildcfg/run.sh
+++ scripts/buildcfg/run.sh
@@ -1,4 +1,5 @@
 #cd /app/Contracts_Testing_Scripts/results/cfgs/research/
 cd ~
 slither $1 --print cfg
-sudo p *.dot /app/Contracts_Testing_Scripts/results/cfgs/research/
+sudo cp *.dot /app/Contracts_Testing_Scripts/results/cfgs/research/
+rm *.dot
