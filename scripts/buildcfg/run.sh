cb712752adcaa238b3f0a7008fc79d3064253f72
Jeongmin Kim <appff13@gmail.com>
2020-06-09 18:28:41 -0700

    Update run.sh to move dot files into repo folder

--- scripts/buildcfg/run.sh
+++ scripts/buildcfg/run.sh
@@ -1,5 +1,4 @@
 #cd /app/Contracts_Testing_Scripts/results/cfgs/research/
 cd ~
 slither $1 --print cfg
-sudo cp *.dot /app/Contracts_Testing_Scripts/results/cfgs/research/
-rm *.dot
+sudo mv *.dot /app/Contracts_Testing_Scripts/results/cfgs/research/
